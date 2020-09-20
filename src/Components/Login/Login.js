2848d26903377460aab69933ddb25f8cb9c2a6c7
Anuragtech02 <apal895@gmail.com>
2020-09-20 15:05:02 +0530

    Create boilerplate - index, app, login and auth

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Login.module.css";
+
+const Login = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.heading}></div>
+    </div>
+  );
+};
+
+export default Login;
